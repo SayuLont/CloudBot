eb6ee83536504a146ba0730b80fbd10a954938ae
SayuLont <99880401+SayuLont@users.noreply.github.com>
2022-05-10 15:52:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281224863098'],
+  ['6285215319934'],
   ['0'],
-  ['6281224863098', 'Della', true]
+  ['6281224863098', 'Ikyii', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'Created By'
-global.author = 'Cloud Bot'
+global.author = 'Ikyii Ads'
 
 global.multiplier = 69 // The higher, The harder levelup
 
